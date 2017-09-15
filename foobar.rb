9437acb0318b534b5106e96c0806e526b1c41179
Victoria <31418877+victorialn@users.noreply.github.com>
2017-09-15 02:49:39 -0700

    Update foobar.rb

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-    a.map!{ |b| b.to_i}.map!{|c| c+2}.keep_if{|d| d.even?}.uniq.delete_if{|e| e>10}.sum
+    a.map!{ |b| b.to_i}.map!{|c| c+2}.keep_if{|d| d.even?}.uniq.delete_if{|e| e>10}.inject(:+)
   end
 end
 
